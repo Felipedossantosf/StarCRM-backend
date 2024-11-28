bebc47658642eee7ea0689bec311f9596639e7a3
Felipe dos Santos <felipe28092001@gmail.com>
2024-11-28 13:30:27 -0300

    Swagger agregado a entorno de produccion

--- StarCRM-backend/WebAPI/Program.cs
+++ StarCRM-backend/WebAPI/Program.cs
@@ -79,7 +79,7 @@ namespace WebAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
