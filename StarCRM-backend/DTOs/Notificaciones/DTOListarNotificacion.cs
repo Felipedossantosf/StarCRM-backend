d4092fb4067adeb68583e6bef77645aa6aa61377
Felipe dos Santos <felipe28092001@gmail.com>
2025-01-06 13:01:01 -0300

    getNotificacionesDeUsuario fixed

--- StarCRM-backend/DTOs/Notificaciones/DTOListarNotificacion.cs
+++ StarCRM-backend/DTOs/Notificaciones/DTOListarNotificacion.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DTOs.Notificaciones
 {
-    public class DTONotificacion
+    public class DTOListarNotificacion
     {
-        
-        public int id { get; set; }
+        public int notificacion_usuario_id { get; set; }
+        public int notificacion_id { get; set; }
         public string mensaje { get; set; }
-        public  DateTime fecha { get; set; }
+        public DateTime fecha { get; set; }
         public int? cliente_id { get; set; }
         public bool? activa { get; set; }
     }
